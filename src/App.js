d201603d642e2ebd3694dfefa6802083a41e024d
Jerry Zhong <jerryzhong08@gmail.com>
2020-10-29 11:16:19 -0400

    Change s_rows to s_convertedRows and add mertWithConvertedRows; move convertor and selector objects out of App component to fix the useState bug on s_rows.

--- src/App.js
+++ src/App.js
@@ -20,52 +20,141 @@ import SampleAvatar10 from './images/SampleAvatar10.png';
 import SampleAvatar11 from './images/SampleAvatar11.png';
 import SampleAvatar12 from './images/SampleAvatar12.png';
 
+const avatars = {
+  avatar0: SampleAvatar0,
+  avatar1: SampleAvatar1,
+  avatar2: SampleAvatar2,
+  avatar3: SampleAvatar3,
+  avatar4: SampleAvatar4,
+  avatar5: SampleAvatar5,
+  avatar6: SampleAvatar6,
+  avatar7: SampleAvatar7,
+  avatar8: SampleAvatar8,
+  avatar9: SampleAvatar9,
+  avatar10: SampleAvatar10,
+  avatar11: SampleAvatar11,
+  avatar12: SampleAvatar12,
+};
+
+const rowDefault = {
+  suffix: 'Mr.',
+  firstName: '',
+  lastName: '',
+  email: '',
+  birthDate: '',
+  gender: 'Male',
+  roles: ['Supplier'],
+  contacts: [],
+  country: '',
+  city: '',
+  currency: 'USD',
+  asset: 0,
+  debt: 0,
+  netAsset: 0,
+};
+
+const currencies = {
+  USD: 1,
+  AUD: 0.71,
+  BRL: 0.18,
+  CZK: 0.043,
+  DKK: 0.16,
+  ECD: 0.37,
+  EUR: 1.17,
+  GBP: 1.28,
+  HKD: 0.13,
+  JPY: 0.0095,
+  LYD: 0.73,
+  RMB: 0.15,
+  RUB: 0.013,
+  TVD: 0.74,
+};
+
+const currencyConverter = {
+  convert(sourceAmount, source, target) {
+    if (source === target) return sourceAmount;
+    return (sourceAmount * currencies[source] / currencies[target]).toFixed(2) * 1.0;
+  },
+  getOptionList() { return Object.keys(currencies); },
+  handleConvert(event, row) {
+    const assetNew = this.convert(row.asset, row.currency, event.target.value);
+    const debtNew = this.convert(row.debt, row.currency, event.target.value);
+    const netAssetNew = this.convert(row.netAsset, row.currency, event.target.value);
+    this.callback({ ...row, 
+      currency : event.target.value,
+      asset: assetNew,
+      debt: debtNew,
+      netAsset: netAssetNew,
+    });
+  },
+};
+
+const regionSelector = {
+  regions: {
+    'USA': ['Atlanta', 'Boston', 'Dallas', 'Denver', 'Los Angales', 'New York'],
+    'Australia': ['Melbourne', 'Sydney'],
+    'Brazil': ['Rio de', 'Sao Paulo'],
+    'Switzerland': ['Basel', 'Geneva', 'Zurich'],
+    'China': ['Beijing', 'Shanghai', 'Hongkong'],
+    'Czechia': ['Brno', 'Prague'],
+    'German': ['Berlin', 'Frankfurt', 'Munich'],
+    'Spain': ['Barcelona', 'Granada', 'Madrid'],
+    'France': ['Marseille', 'Nice', 'Paris'],
+    'Greenland': ['Nuuk','Sisimiut'],
+    'Italy': ['Florence', 'Milan', 'Rome'],
+    'Grenada': ['Bogles', 'Tivoli'],
+    'Japan': ['Kyoto', 'Osaka', 'Tokyo'],
+    'Libya': ['Benghzai', 'Tripoli'],
+    'Russia': ['Moscow', 'Saint Petersburg'],
+    'England': ['Liverpool', 'London', 'Manchester'],
+  },
+  currentKey: null,
+  getKeys() { return Object.keys(this.regions); },
+  setKey(key) { this.currentKey = key; },
+  getValues() { return this.currentKey ? this.regions[this.currentKey] : []; },
+};
+
+const countrySelector = {
+  getOptionList() { return regionSelector.getKeys(); },
+  handleSelect(value) { regionSelector.setKey(value); },
+}
+
+const citySelector = {
+  getOptionList() { return regionSelector.getValues(); }
+}
+
+const suffixSelector = {
+  getOptionList() { return ['Mr.', 'Mrs.']; },
+};
+
+const genderSelector = {
+  getOptionList() { return ['Male', 'Female', 'Other']; },
+};
+
+const currencySelector = {
+  getOptionList() { return Object.keys(currencies); },
+};
+
+const rolesSelector = {
+  getOptionList() { return ['Supplier', 'Consumer', 'Broker']; },
+};
+
+const contactsSelector = {
+  getOptionList() { return ['Mobile', 'Office', 'Fax']; },
+};
+
 export default function App() {
 
-  const rowDefault = {
-    suffix: 'Mr.',
-    firstName: '',
-    lastName: '',
-    email: '',
-    birthDate: '',
-    gender: 'Male',
-    roles: ['Supplier'],
-    contacts: [],
-    country: '',
-    city: '',
-    currency: 'USD',
-    asset: 0,
-    debt: 0,
-    netAsset: 0,
-  };
-
-  const avatars = {
-    avatar0: SampleAvatar0,
-    avatar1: SampleAvatar1,
-    avatar2: SampleAvatar2,
-    avatar3: SampleAvatar3,
-    avatar4: SampleAvatar4,
-    avatar5: SampleAvatar5,
-    avatar6: SampleAvatar6,
-    avatar7: SampleAvatar7,
-    avatar8: SampleAvatar8,
-    avatar9: SampleAvatar9,
-    avatar10: SampleAvatar10,
-    avatar11: SampleAvatar11,
-    avatar12: SampleAvatar12,
-  };
-
-  // s_users store the data and are affected by user updates (e.g. delete, edit)
+  // s_users store the original data and are affected by user changes (add, delete, update)
   const [s_users, setUsers] = React.useState(users.slice(0, 1000));
-  // const [s_users, setUsers] = React.useState([]);
-  // s_rows reflect data transformation made to the data (e.g. currency convert)
-  const [s_rows, setRows] = React.useState(s_users);
+  // s_convertedRows reflect data conversion made to certain rows (e.g. currency convert)
+  const [s_convertedRows, setConvertedRows] = React.useState([s_users[0]]);
   const [s_columnInfo, setColumnInfo] = React.useState({
     columns: columns,
     columnsHidden: columnsHidden,
     // columnsHidden: [],
-    // groupColumns: groupColumns,
-    groupColumns: [],
+    groupColumns: groupColumns,
+    // groupColumns: [],
   });
 
   const handleSettingsUpdate = (columns, columnsHidden, groupColumns) => {
@@ -81,9 +170,8 @@ export default function App() {
       acc[key] = rowNew[key];
       return acc;
     }, { mutable: true });
-    const usersNew = [userNew, ...s_users];
+    const usersNew = [...s_users, userNew];
     setUsers(usersNew);
-    setRows(usersNew);
   }
 
   const handleUserUpdate = (rowNew) => {
@@ -98,130 +186,59 @@ export default function App() {
         userNew,
         ...s_users.slice(indexFound + 1)
       ];
+      const convertedRowsNew = s_convertedRows.filter(row => row.id !== rowNew.id);
       setUsers(usersNew);
-      setRows(usersNew);
+      setConvertedRows(convertedRowsNew);
     }
   }
 
   const handleUserDelete = (rowId) => {
     const usersNew = s_users.filter(user => user.id !== rowId);
+    const convertedRowsNew = s_convertedRows.filter(row => row.id !== rowId);
     setUsers(usersNew);
-    setRows(usersNew);
+    setConvertedRows(convertedRowsNew);
   }
 
-  const handleRowTransform = (rowNew) => {
-    const indexFound = s_rows.findIndex(row => row.id === rowNew.id);
-    if (indexFound !== -1) {
-      setRows([
-        ...s_rows.slice(0, indexFound),
-        rowNew,
-        ...s_rows.slice(indexFound + 1)
-      ]);
-    }
+  const handleRowConvert = (rowNew) => {
+    const convertedRowsNew = s_convertedRows.filter(row => row.id !== rowNew.id);
+    setConvertedRows([rowNew, ...convertedRowsNew]);
   };
 
-  // eliminate all date transformation made to the data
+  // Eliminate all conversion made to the data
   const handleRowRefresh = () => {
-    setRows(s_users);
+    setConvertedRows([]);
   };
 
-  const currencies = {
-    USD: 1,
-    AUD: 0.71,
-    BRL: 0.18,
-    CZK: 0.043,
-    DKK: 0.16,
-    ECD: 0.37,
-    EUR: 1.17,
-    GBP: 1.28,
-    HKD: 0.13,
-    JPY: 0.0095,
-    LYD: 0.73,
-    RMB: 0.15,
-    RUB: 0.013,
-    TVD: 0.74,
+  const mergeWithConvertedRows = (users, convertedRows) => {
+    const convertedRowMap = convertedRows.reduce((acc, row) => {
+      acc[row.id] = row;
+      return acc;
+    }, {})
+    return users.reduce((acc, user) => {
+      if (user.id in convertedRowMap) acc.push(convertedRowMap[user.id]);
+      else acc.push(user);
+      return acc;
+    }, []);
   };
 
-  const currencyConverter = {
-    convert(sourceAmount, source, target) {
-      if (source === target) return sourceAmount;
-      return (sourceAmount * currencies[source] / currencies[target]).toFixed(2) * 1.0;
-    },
-    getOptionList() { return Object.keys(currencies); },
-    handleConvert(event, row) {
-      const assetNew = this.convert(row.asset, row.currency, event.target.value);
-      const debtNew = this.convert(row.debt, row.currency, event.target.value);
-      const netAssetNew = this.convert(row.netAsset, row.currency, event.target.value);
-      this.callback({ ...row, 
-        currency : event.target.value,
-        asset: assetNew,
-        debt: debtNew,
-        netAsset: netAssetNew,
-      });
-    },
-  };
-  currencyConverter.callback = handleRowTransform;
+  // const mergedRows = mergeWithConvertedRows(s_users, s_convertedRows);
+  const mergedRows = React.useMemo(
+    () => {
+      return mergeWithConvertedRows(s_users, s_convertedRows)
+    }, [s_users, s_convertedRows]
+  );
+
+  // Attach converters
+  currencyConverter.callback = handleRowConvert;
   s_columnInfo.columns.currency.converter = currencyConverter;
-
-  const regionSelector = {
-    regions: {
-      'USA': ['Atlanta', 'Boston', 'Dallas', 'Denver', 'Los Angales', 'New York'],
-      'Australia': ['Melbourne', 'Sydney'],
-      'Brazil': ['Rio de', 'Sao Paulo'],
-      'Switzerland': ['Basel', 'Geneva', 'Zurich'],
-      'China': ['Beijing', 'Shanghai', 'Hongkong'],
-      'Czechia': ['Brno', 'Prague'],
-      'German': ['Berlin', 'Frankfurt', 'Munich'],
-      'Spain': ['Barcelona', 'Granada', 'Madrid'],
-      'France': ['Marseille', 'Nice', 'Paris'],
-      'Greenland': ['Nuuk','Sisimiut'],
-      'Italy': ['Florence', 'Milan', 'Rome'],
-      'Grenada': ['Bogles', 'Tivoli'],
-      'Japan': ['Kyoto', 'Osaka', 'Tokyo'],
-      'Libya': ['Benghzai', 'Tripoli'],
-      'Russia': ['Moscow', 'Saint Petersburg'],
-      'England': ['Liverpool', 'London', 'Manchester'],
-    },
-    currentKey: null,
-    getKeys() { return Object.keys(this.regions); },
-    setKey(key) { this.currentKey = key; },
-    getValues() { return this.currentKey ? this.regions[this.currentKey] : []; },
-  };
   
-  const countrySelector = {
-    getOptionList() { return regionSelector.getKeys(); },
-    handleSelect(value) { regionSelector.setKey(value); },
-  }
+  // Attach selectors
   s_columnInfo.columns.country.selector = countrySelector;
-
-  const citySelector = {
-    getOptionList() { return regionSelector.getValues(); }
-  }
   s_columnInfo.columns.city.selector = citySelector;
-
-  const suffixSelector = {
-    getOptionList() { return ['Mr.', 'Mrs.']; },
-  };
   s_columnInfo.columns.suffix.selector = suffixSelector;
-
-  const genderSelector = {
-    getOptionList() { return ['Male', 'Female', 'Other']; },
-  };
   s_columnInfo.columns.gender.selector = genderSelector;
-
-  const currencySelector = {
-    getOptionList() { return Object.keys(currencies); },
-  };
   s_columnInfo.columns.currency.selector = currencySelector;
-
-  const rolesSelector = {
-    getOptionList() { return ['Supplier', 'Consumer', 'Broker']; },
-  };
   s_columnInfo.columns.roles.selector = rolesSelector;
-
-  const contactsSelector = {
-    getOptionList() { return ['Mobile', 'Office', 'Fax']; },
-  };
   s_columnInfo.columns.contacts.selector = contactsSelector;
 
   return (
@@ -234,18 +251,18 @@ export default function App() {
           title: 'New Users',
           desc: 'users joined since start of year',
           width: '98vw',
-          height: '65vh',
+          height: '92vh',
         }}
         columns={s_columnInfo.columns}
         columnsHidden={s_columnInfo.columnsHidden}
         groupColumns={s_columnInfo.groupColumns}
         defaultRow={rowDefault}
         // defaultRow={{}}
-        rows={s_rows}
+        rows={mergedRows}
         pieCharts={{ 'investments': investments }}
         images={{ 'avatar': avatars }}
-        // rowsPerPageOptions={[15, 25, 50]}
-        useVirtualization={true}
+        rowsPerPageOptions={[15, 25, 50]}
+        // useVirtualization={true}
         showStats={'top'}
         onRowUpdated={handleUserUpdate}
         onRowDeleted={handleUserDelete}
@@ -254,10 +271,7 @@ export default function App() {
         onRowRefresh={handleRowRefresh}
         enableExport={true}
         showTopToolbar={true}
-        // key={'users'}
       />
     </div>
   );
 }
-
-// export default App
